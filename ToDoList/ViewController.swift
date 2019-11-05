d1b655ff1a393d6350928ac431f69fff1b2afdaa
yilinzhong <yzhong@lincolnschool.org>
2019-11-05 09:19:47 -0500

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Soft Dev on 11/5/19.
+//  Copyright © 2019 Alice Zhong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
